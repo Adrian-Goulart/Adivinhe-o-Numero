f4c4eb7745f81283e95633d4716d21984305a4ab
Adrian-Goulart <adrianx2007@hotmail.com>
2026-04-13 17:24:06 -0300

    Adicionado lista de números já utilizados e uma tratativa ao inserir valor nulo, strings e números negativos ou excedentes

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -2,12 +2,15 @@ package Game;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Game {
     private final Scanner sc = new Scanner(System.in);
     private int choice;
-    private int tries = 0;
+    private int answer;
+    private int tries = 1;
     private int triesLimit = 5;
     private boolean cheat = false;
     private boolean isTriesLimitActive = false;
@@ -19,10 +22,8 @@ public class Game {
     public void gameStart() {
         boolean gameStartMenu = true;
         while (gameStartMenu) {
-            System.out.println("--Início--");
-            System.out.println("1 - Modos de Jogo");
-            System.out.println("2 - Configuração");
-            checkScanner();
+            System.out.println("--Início--\n1 - Modos de Jogo\n2 - Configuração");
+            choiceInt();
 
             switch (choice) {
                 case 0 -> gameStartMenu = false;
@@ -41,16 +42,11 @@ public class Game {
 
     public void gameModeSelect() {
         boolean gameModeMenu = true;
-        this.tries = 0;
+        this.tries = 1;
 
         while (gameModeMenu) {
-            System.out.println("--Modos de Jogo--");
-            System.out.println("1 - Padrão (1 - 100)");
-            System.out.println("2 - Médio (1 - 1000)");
-            System.out.println("3 - Difícil? (1 - 10000)");
-            System.out.println("4 - Personalizado");
-            System.out.println("0 - Voltar");
-            checkScanner();
+            System.out.println("--Modos de Jogo--\n1 - Padrão (1 - 100)\n2 - Médio (1 - 1000)\n3 - Difícil? (1 - 10000)\n4 - Personalizado\n0 - Voltar");
+            choiceInt();
 
             gameModeMenu = switch (choice) {
                 case 0 -> {
@@ -79,18 +75,24 @@ public class Game {
     }
 
     public void gameMode(int numMin, int numMax) {
-        int answer;
-        int range = (numMax - numMin) + 1;
-        int randomNum = (int) (Math.random() * range) + numMin;
+        int randomNum = generateNumber(numMin, numMax);
+        List<Integer> saveAnswers = new ArrayList<>();
 
-        System.out.println("De " + numMin + " a " + numMax);
-        System.out.println("Numero gerado, qual seu chute?");
+        System.out.println("De " + numMin + " a " + numMax + "\nNumero gerado, qual seu chute?");
         System.out.println((cheat) ? "colinha: " + randomNum : "");
 
         Instant startTime = Instant.now();
         do {
-            answer = Integer.parseInt(sc.nextLine());
-            tries++;
+            answerInt(numMax);
+
+            if (!saveAnswers.contains(answer)) {
+                saveAnswers.add(answer);
+            } else {
+                System.out.println("Já utilizou este número");
+                tries -= 1;
+            }
+
+            // Está feio
             if (answer == randomNum) {
                 Instant endTime = Instant.now();
                 long time = Duration.between(startTime, endTime).getSeconds();
@@ -98,11 +100,10 @@ public class Game {
                 System.out.println("Acertou!");
                 System.out.println("Com: " + tries + " " + writingTryOrTries);
                 System.out.println("Tempo: " + time + " segundos");
-            } else if (answer < randomNum) {
-                System.out.println("O número é maior que: " + answer);
             } else {
-                System.out.println("O número é menor que: " + answer);
+                incorrectAnswer(answer, randomNum, saveAnswers);
             }
+            tries++;
         } while (answer != randomNum && tryLimitExpression(randomNum));
     }
 
@@ -110,9 +111,7 @@ public class Game {
         boolean running = true;
         do {
             int personalizedMaxValue;
-            System.out.println("--Modo Personalizado--");
-            System.out.println("O valor mínimo por padrão é 1");
-            System.out.println("Qual será o valor máximo?");
+            System.out.println("--Modo Personalizado--\nO valor mínimo por padrão é 1\nQual será o valor máximo?");
 
             try {
                 personalizedMaxValue = Integer.parseInt(sc.nextLine());
@@ -127,11 +126,8 @@ public class Game {
     public void gameConfigurations() {
         boolean gameConfigurationsMenu = true;
         while (gameConfigurationsMenu) {
-            System.out.println("--Configurações--");
-            System.out.println("1 - Cheat");
-            System.out.println("2 - Limite de tentativas");
-            System.out.println("0 - Voltar");
-            checkScanner();
+            System.out.println("--Configurações--\n1 - Cheat\n2 - Limite de tentativas\n0 - Voltar");
+            choiceInt();
 
             gameConfigurationsMenu = switch (choice) {
                 case 0 -> {
@@ -151,21 +147,13 @@ public class Game {
         }
     }
 
-    public void toggleCheat() {
-        cheat = !cheat;
-        System.out.println("Cheat: " + cheat);
-        gameStart();
-    }
-
     public void triesLimitConfiguration() {
         boolean triesLimitConfigurationMenu = true;
         while (triesLimitConfigurationMenu) {
             System.out.println("--Tentativas Configuração--");
             System.out.println("Quantidade de tentativas: " + ((isTriesLimitActive) ? "Ativo" : "Desativado") + " | Quantidade: " + triesLimit);
-            System.out.println("1 - Ativar/Desativar");
-            System.out.println("2 - Escolher quantidade (Padrão 5)");
-            System.out.println("0 - Voltar");
-            checkScanner();
+            System.out.println("1 - Ativar/Desativar\n2 - Escolher quantidade (Padrão 5)\n0 - Voltar");
+            choiceInt();
 
             triesLimitConfigurationMenu = switch (choice) {
                 case 0 -> {
@@ -192,20 +180,54 @@ public class Game {
     public boolean tryLimitExpression(int randomNum) {
         if (isTriesLimitActive) {
             if (tries == triesLimit) {
-                System.out.println("Acabou as tentativas!");
-                System.out.println("O número era: " + randomNum);
+                System.out.println("Acabou as tentativas!\nO número era: " + randomNum);
                 return false;
             }
         }
         return true;
     }
 
-    public void checkScanner() {
+    public void toggleCheat() {
+        cheat = !cheat;
+        System.out.println("Cheat: " + cheat);
+        gameStart();
+    }
+
+    public void choiceInt() {
         try {
             choice = Integer.parseInt(sc.nextLine());
         } catch (NumberFormatException e) {
             System.out.println("Formato inválido, por favor insira um número.");
             choice = -1;
         }
+    }
+
+    public void answerInt(int numMax) {
+        try {
+            do {
+                answer = Integer.parseInt(sc.nextLine());
+            } while (validAnswer(numMax));
+        } catch (Exception e) {
+            System.out.println("Formato inválido, por favor insira um número inteiro.");
+        }
+    }
+
+    public boolean validAnswer(int numMax) {
+        if (answer < 1 || answer > numMax) {
+            System.out.println("Número excedente, maior que o máximo ou menor que o mínimo\nDigite um número válido novamente");
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void incorrectAnswer(int answer, int randomNum, List<Integer> saveAnswers) {
+        System.out.println((answer > randomNum) ? "O número é menor que: " + answer : "O número é maior que: " + answer);
+        System.out.println("Tentativa de número: " + tries + "\nNúmeros utilizados: " + saveAnswers);
+    }
+
+    public int generateNumber(int numMin, int numMax) {
+        int range = (numMax - numMin) + 1;
+        return (int) (Math.random() * range) + numMin;
     }
 }
--- src/Game/nsei.md
+++ src/Game/nsei.md
@@ -1,5 +1,7 @@
-Vi que a chamada de métodos não é algo recomendado para menus, então posso utilizar o while
+Vi que a chamada dos métodos não é algo recomendado para menus, então posso utilizar o while
 
 Tentei melhorar o código acho que consegui e também tentei fazer uma tentativa de colocar limite por tempo, mas nn consegui então parei por hora
 
 Em questão da memória, sobre chamar um método sendo que o outro ainda não foi finalizado, ainda não sei o que fazer sobre isto
+
+As vezes tenho de pressionar _enter_ duas vezes
